9e637d9d6c7f758bfcc5ddb701f36cf2c62bfa73
Tobias Seibel <55710042+SeibelT@users.noreply.github.com>
2022-01-24 09:40:51 +0100

    Update sl-node.sh

--- System A/swarm_setup_training/sl-node.sh
+++ System A/swarm_setup_training/sl-node.sh
@@ -3,15 +3,15 @@ sudo docker rm sl-1
 bash ./swarm-learning/bin/run-sl        \
     --name=sl-1                         \
     --sl-platform=pyt-cv2                   \
-    --host-ip= system_A_ip                  \
+    --host-ip= <system_A_ip>                  \
     --sn-ip= system_A_ip                  \
     --sn-api-port=11000                 \
     --sl-fs-port=16000                  \
-    --data-dir=path-to-data-dir \
-    --model-dir=path-to-model-dir-having-main-pathon-file/MODEL    \
+    --data-dir=<path-to-data-dir> \
+    --model-dir=<path-to-model-dir-having-main-pathon-file/MODEL>    \
     --model-program=main.py        \
     --gpu=0                             \
-    --apls-ip  system_A_ip               \
-    -serverAddress system_A_ip            \
+    --apls-ip  <system_A_ip>               \
+    -serverAddress <system_A_ip>            \
     -genJoinToken
 
