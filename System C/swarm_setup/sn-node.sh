936e6834a284e55d591dd04e63c861e611d63f67
Tobias Seibel <55710042+SeibelT@users.noreply.github.com>
2022-01-24 14:04:43 +0100

    Update sn-node.sh

--- System C/swarm_setup/sn-node.sh
+++ System C/swarm_setup/sn-node.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 sudo docker rm sn-3
-system_A_ip = <system_A_ip>
+#insert variables here:
+system_A_ip =<system_A_ip>
+#####
 system_C_ip =$(hostname  -I | cut -f1 -d' ')
 bash ./swarm-learning/bin/run-sn  \
     --name=sn-1              \
-    --host-ip=system_C_ip       \
-    --sentinel-ip=system_A_ip   \
+    --host-ip=$system_C_ip       \
+    --sentinel-ip=$system_A_ip   \
     --sn-p2p-port=10000      \
     --sn-api-port=11000      \
     --sn-fs-port=12000       \
-    --apls-ip system_A_ip       \
-    -serverAddress system_A_ip  \
+    --apls-ip $system_A_ip       \
+    -serverAddress $system_A_ip  \
     -genJoinToken
 
