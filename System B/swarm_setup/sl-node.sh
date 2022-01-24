5fdf79f0a3cf8ac77345bdce12d283cccc2caf47
Tobias Seibel <55710042+SeibelT@users.noreply.github.com>
2022-01-24 14:01:05 +0100

    Update sl-node.sh

--- System B/swarm_setup/sl-node.sh
+++ System B/swarm_setup/sl-node.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 sudo docker rm sl-2
+#insert variables here:
 system_A_ip=<system_A_ip>
-data_directory = <path-to-data-dir>
+data_directory =<path-to-data-dir>
 model_directory = <path-to-model-dir-having-main-pathon-file/MODEL>
+######
 system_B_ip =$(hostname  -I | cut -f1 -d' ')
 bash ./swarm-learning/bin/run-sl        \
     --name=sl-1                         \
     --sl-platform=pyt-cv2                   \
-    --host-ip=system_B_ip                 \
-    --sn-ip=system_B_ip                    \
+    --host-ip=$system_B_ip                 \
+    --sn-ip=$system_B_ip                    \
     --sn-api-port=11000                 \
     --sl-fs-port=16000                  \
-    --data-dir=data_directory  \
-    --model-dir=model_directory    \
+    --data-dir=$data_directory  \
+    --model-dir=$model_directory    \
     --model-program=main.py        \
     --gpu=0                             \
-    --apls-ip system_A_ip                 \
-    -serverAddress system_A_ip            \
+    --apls-ip $system_A_ip                 \
+    -serverAddress $system_A_ip            \
     -genJoinToken
 
