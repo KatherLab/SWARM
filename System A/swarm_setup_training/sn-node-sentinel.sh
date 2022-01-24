a459f12058a31b32bda3a70611143a42192c1785
Tobias Seibel <55710042+SeibelT@users.noreply.github.com>
2022-01-24 13:59:45 +0100

    Update sn-node-sentinel.sh

--- System A/swarm_setup_training/sn-node-sentinel.sh
+++ System A/swarm_setup_training/sn-node-sentinel.sh
@@ -3,12 +3,12 @@ sudo docker rm sn-1
 system_A_ip =$(hostname  -I | cut -f1 -d' ')
 bash ./swarm-learning/bin/run-sn  \
     --name=sn-1              \
-    --host-ip=system_A_ip       \
-    --sentinel-ip=system_A_ip   \
+    --host-ip=$system_A_ip       \
+    --sentinel-ip=$system_A_ip   \
     --sn-p2p-port=10000      \
     --sn-api-port=11000      \
     --sn-fs-port=12000       \
-    --apls-ip system_A_ip       \
-    -serverAddress system_A_ip  \
+    --apls-ip $system_A_ip       \
+    -serverAddress $system_A_ip  \
     -genJoinToken
 
